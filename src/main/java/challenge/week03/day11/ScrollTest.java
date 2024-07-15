87dbf9ab2471ccd78e421c7a4bdf8677b8d9b3b6
Karthikeyan Rajendran <Karthikeyan_Rajendran@epam.com>
2024-07-15 06:32:30 +0200

    Initial Code ppush

--- src/main/java/challenge/week03/day11/ScrollTest.java
+++ src/main/java/challenge/week03/day11/ScrollTest.java
@@ -2,8 +2,11 @@ package challenge.week03.day11;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -22,7 +25,7 @@ public class ScrollTest {
 		driver = new ChromeDriver(options);
 		wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		driver.manage().window().maximize();
-		driver.get("https://login.salesforce.com/");
+		driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 	}
 
@@ -33,7 +36,21 @@ public class ScrollTest {
 	
 	@Test
 	public void scrollTest() {
-		
-	}	
+		WebElement iframe = driver.findElement(By.tagName("iframe"));
+		inViewport(iframe);
+		new Actions(driver).scrollToElement(iframe).perform();
+		inViewport(iframe);
+	}
+	
+	private void inViewport(WebElement element) {
+
+        String script =
+                "for(var e=arguments[0],f=e.offsetTop,t=e.offsetLeft,o=e.offsetWidth,n=e.offsetHeight;\n"
+                        + "e.offsetParent;)f+=(e=e.offsetParent).offsetTop,t+=e.offsetLeft;\n"
+                        + "return f<window.pageYOffset+window.innerHeight&&t<window.pageXOffset+window.innerWidth&&f+n>\n"
+                        + "window.pageYOffset&&t+o>window.pageXOffset";
+
+        System.out.println(driver.executeScript(script, element));
+    }
 
 }
