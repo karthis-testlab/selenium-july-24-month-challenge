2ac49f8b5fd6b3745006f746eaaaf0b20458e2b2
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-07-18 00:05:42 +0530

    18/07/2024:12:05AM

--- src/main/java/challenge/week03/day14/WindowSwtichTest.java
+++ src/main/java/challenge/week03/day14/WindowSwtichTest.java
@@ -1,5 +1,40 @@
 package challenge.week03.day14;
 
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class WindowSwtichTest {
+	
+	ChromeDriver driver;
+	WebDriverWait wait;
+	
+	@BeforeMethod
+	public void setUp() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		options.addArguments("--incognito");
+		driver = new ChromeDriver(options);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.navigate().to("https://login.salesforce.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) {
+		driver.close();
+	}
+	
+	@Test
+	public void switchWindowTest() {
+		
+	}
 
 }
