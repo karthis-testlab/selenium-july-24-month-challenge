57cd9743e625cfd548b84924b56d9d235ab0e5e2
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-07-26 00:08:07 +0530

    26/07/2024:12:08:00AM

---
+++ src/main/java/challenge/week04/day20/BasicAuthenticationTest.java
@@ -0,0 +1,29 @@
+package challenge.week04.day20;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BasicAuthenticationTest {
+	
+	ChromeDriver driver;
+	WebDriverWait wait;
+	
+	@BeforeMethod
+	public void setUp() {
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void testBasicAuthenticationLogic() {
+		
+	}
+
+}
